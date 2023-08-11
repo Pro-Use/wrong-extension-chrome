4b978baec27c124fad58dc36c109511a0a27f1af
Pro-Use <robertprouse@gmail.com>
2023-08-11 12:45:51 +0100

    Current project shows below main window with checks for live

--- archive.js
+++ archive.js
@@ -1,3 +1,4 @@
+const liveContainer = document.getElementById('liveContainer');
 const archiveContainer = document.getElementById('archiveContainer');
 const projectContainer = document.getElementById('projectContainer');
 let archiveData;
@@ -16,14 +17,25 @@ fetch(archive_url, {mode: 'cors'})
   .catch(err => console.log(err))
 
 
-  function buildArchive(jsonArray) {
+  async function buildArchive(jsonArray) {
+    let { project } = await chrome.storage.local.get('project');
     let index = 0;
+    let header = document.createElement("h2");
+    header.classList.add('box-header')
+    header.innerHTML = 'Previous Projects';
+    archiveContainer.appendChild(header)
     jsonArray.forEach(function(item) {
       var title = item.title;
       var curator = item.curator;
       var from = item.from;
       var img = item.img || "/placeholder.svg";
       from = from.replace(/-/g, '.');
+      if(project && project.slug && item.project == project.slug){
+        let live_header = document.createElement("h2");
+        live_header.classList.add('box-header')
+        live_header.innerHTML = 'Current Project';
+        liveContainer.appendChild(live_header)
+      }
       var element = document.createElement("div");
       element.innerHTML = `
       <a class="archive-item info-item red-shadow box" href="#project" data-index="${index}">
@@ -43,13 +55,29 @@ fetch(archive_url, {mode: 'cors'})
         </div>
       </a>
       `;
-      archiveContainer.appendChild(element);
+      if (project && project.slug && item.project == project.slug){
+        liveContainer.appendChild(element);
+      } else if (!item.live) {
+        archiveContainer.appendChild(element);
+      }
+      
       
       index++;
     });
   }
 
   archiveContainer.addEventListener('click', (e) => {
+    e.preventDefault();
+    if (e.target.classList.contains('archive-item')) {
+        e.preventDefault();
+        console.log(window.pageXOffset);
+        let index = e.target.dataset.index;
+        buildProjectPage(archiveData[index]);
+        openProjectPage(e);
+    }
+  });
+
+  liveContainer.addEventListener('click', (e) => {
     e.preventDefault();
     if (e.target.classList.contains('archive-item')) {
         e.preventDefault();
@@ -69,7 +97,7 @@ fetch(archive_url, {mode: 'cors'})
       var popups = item.popups;
       from = from.replace(/-/g, '.');
       var element = document.createElement("div");
-      element.innerHTML = `
+      let new_html = `
         <img srcset="${img}" class="project-image" />
         <div class="project-main padding">
             <div class="project-info-box red-shadow box bg-white">
@@ -84,15 +112,24 @@ fetch(archive_url, {mode: 'cors'})
                     </div>
                 </details>
             </div>
+            
+      `;
+      if (!item.live){
+        new_html += `
             <div class="project-popups">
                 <h3 class="box-header">Works:</h3>
                 <div id="popupContainer"></div>
             </div>
-        </div>
-      `;
-      // Append load button if not current project
+        </div>`
+      } else {
+        new_html += `
+        </div>`
+      }
+      element.innerHTML = new_html;
+      // Append load button if not current project and not live
       let { project } = await chrome.storage.local.get('project');
       if (!project || project.slug && project.slug != item.project){
+        if(!item.live){
           let load = element.querySelector('#load-button')
           let project_button = document.createElement('button');
           project_button.classList.add('project-button', 'box', 'bg-white');
@@ -106,6 +143,7 @@ fetch(archive_url, {mode: 'cors'})
             port.postMessage(msg);
             window.close();
           });
+        }
       // Append unload button if current but not live
       } else if (project && project.slug == item.project && !item.live){
           let load = element.querySelector('#load-button')
@@ -122,64 +160,66 @@ fetch(archive_url, {mode: 'cors'})
       //
       projectContainer.innerHTML = "";
       projectContainer.appendChild(element);
-      for (let i = 0; i < popups.length; i++) {
-        allPopups[popups[i].id] = popups[i];
-        let popupTitle = popups[i].popup_title;
-        let popupTime =  popups[i].popup_time || "HH:MM:SS";
-        let popupDay = popups[i].popup_day || "1";
-        let button = document.createElement('button');
-        button.classList.add('popup-button', 'box', 'bg-white');
-        button.dataset.popupid = popups[i].id;
-        button.innerHTML = `
-            <span class="button-title">${popupTitle}</span>
-            <span class="button-times">Day ${popupDay} – ${popupTime}</span>
-            <svg width="24" height="24" viewBox="0 0 33 33" preserveAspectRatio="xMidYMid meet">
-              <g id="Layer_1-2"><path d="m32,0H12c-.55,0-1,.45-1,1v20c0,.55.45,1,1,1h20c.55,0,1-.45,1-1V1c0-.55-.45-1-1-1Zm-1,20H14.41l5.59-5.59v3.59h2v-6c0-.55-.45-1-1-1h-6v2h3.59l-5.59,5.59V2h18v18Z" fill="blue"/><path d="m20,31H2V13h7v-2H1c-.55,0-1,.45-1,1v20c0,.55.45,1,1,1h20c.55,0,1-.45,1-1v-8h-2v7Z" fill="blue"/></g>
-            </svg>
-        `;
-        document.getElementById('popupContainer').appendChild(button);
-        button.addEventListener("click", function () {
-            // e.preventDefault();
-            let id = this.dataset.popupid;
-            let popup_info = allPopups[id];
-            // console.log(popup_info)
-            // info window
-            let info_height = screen.height;
-            let info_width = 450;
-            let info_top = 0;
-            let info_left = parseInt(screen.width-info_width);
-            let popup_json = JSON.stringify({dims:[info_left,info_top,info_width,info_height], fullscreen:false, url:popup_info.info_url});
-            port.postMessage(popup_json);
-            // popup
-            height = parseInt(popup_info.height);
-            width = parseInt(popup_info.width);
-            dims = []
-            if (popup_info.fullscreen != 'true'){
-                pos_arr = popup_info.position.split("-");
-        //                horizontal
-                if (pos_arr[1] === "left") {
-                    left = 0;
-                } else if (pos_arr[1] === "center") {
-                    left = parseInt((screen.width-width)/2);
-                } else if (pos_arr[1] === "right"){
-                    left = screen.width-width;
+      if (!item.live){
+          for (let i = 0; i < popups.length; i++) {
+            allPopups[popups[i].id] = popups[i];
+            let popupTitle = popups[i].popup_title;
+            let popupTime =  popups[i].popup_time || "HH:MM:SS";
+            let popupDay = popups[i].popup_day || "1";
+            let button = document.createElement('button');
+            button.classList.add('popup-button', 'box', 'bg-white');
+            button.dataset.popupid = popups[i].id;
+            button.innerHTML = `
+                <span class="button-title">${popupTitle}</span>
+                <span class="button-times">Day ${popupDay} – ${popupTime}</span>
+                <svg width="24" height="24" viewBox="0 0 33 33" preserveAspectRatio="xMidYMid meet">
+                  <g id="Layer_1-2"><path d="m32,0H12c-.55,0-1,.45-1,1v20c0,.55.45,1,1,1h20c.55,0,1-.45,1-1V1c0-.55-.45-1-1-1Zm-1,20H14.41l5.59-5.59v3.59h2v-6c0-.55-.45-1-1-1h-6v2h3.59l-5.59,5.59V2h18v18Z" fill="blue"/><path d="m20,31H2V13h7v-2H1c-.55,0-1,.45-1,1v20c0,.55.45,1,1,1h20c.55,0,1-.45,1-1v-8h-2v7Z" fill="blue"/></g>
+                </svg>
+            `;
+            document.getElementById('popupContainer').appendChild(button);
+            button.addEventListener("click", function () {
+                // e.preventDefault();
+                let id = this.dataset.popupid;
+                let popup_info = allPopups[id];
+                // console.log(popup_info)
+                // info window
+                let info_height = screen.height;
+                let info_width = 450;
+                let info_top = 0;
+                let info_left = parseInt(screen.width-info_width);
+                let popup_json = JSON.stringify({dims:[info_left,info_top,info_width,info_height], fullscreen:false, url:popup_info.info_url});
+                port.postMessage(popup_json);
+                // popup
+                height = parseInt(popup_info.height);
+                width = parseInt(popup_info.width);
+                dims = []
+                if (popup_info.fullscreen != 'true'){
+                    pos_arr = popup_info.position.split("-");
+            //                horizontal
+                    if (pos_arr[1] === "left") {
+                        left = 0;
+                    } else if (pos_arr[1] === "center") {
+                        left = parseInt((screen.width-width)/2);
+                    } else if (pos_arr[1] === "right"){
+                        left = screen.width-width;
+                    }
+            //                vertical
+                    if (pos_arr[0] === "top"){
+                         popup_top = 0;
+                    } else if (pos_arr[0] === "mid") {
+                         popup_top = screen.availHeight - height;
+                         popup_top = parseInt(popup_top / 2);
+                    } else if (pos_arr[0] === "bottom"){
+                         popup_top = screen.height-height;
+                    }
+                    dims.push(left, popup_top, width, height);
                 }
-        //                vertical
-                if (pos_arr[0] === "top"){
-                     popup_top = 0;
-                } else if (pos_arr[0] === "mid") {
-                     popup_top = screen.availHeight - height;
-                     popup_top = parseInt(popup_top / 2);
-                } else if (pos_arr[0] === "bottom"){
-                     popup_top = screen.height-height;
-                }
-                dims.push(left, popup_top, width, height);
-            }
-            msg = JSON.stringify({dims:dims, fullscreen:(popup_info.fullscreen == 'true'), url:popup_info.url});
-            // console.log(msg);
-            port.postMessage(msg);
-        });
-      }
+                msg = JSON.stringify({dims:dims, fullscreen:(popup_info.fullscreen == 'true'), url:popup_info.url});
+                // console.log(msg);
+                port.postMessage(msg);
+            });
+          }
+        }
   }
 
   function openProjectPage(event){
